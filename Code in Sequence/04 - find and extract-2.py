19b0ed9384ed4b305d86a04b3288dcbb78e762ab
Bakar31 <abubakar1808031@gmail.com>
2022-01-22 21:59:12 +0600

    day-03

--- Code in Sequence/04 - find and extract-2.py
+++ Code in Sequence/04 - find and extract-2.py
@@ -1,4 +1,3 @@
-from markupsafe import string
 import requests
 from bs4 import BeautifulSoup
 import re
---
+++ Code in Sequence/05 - Project-1.py
@@ -0,0 +1,22 @@
+# Scrape all product detail and store in a pandas dataframe
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = 'https://webscraper.io/test-sites/e-commerce/allinone/phones/touch'
+page = requests.get(url)
+soup = BeautifulSoup(page.text, 'lxml')
+
+names = soup.find_all('a', {'class': 'title'})
+prices = soup.find_all('h4', {'class':'pull-right price'})
+reviews = soup.find_all('p', {'class':'pull-right'})
+descriptions = soup.find_all('p', {'class':'description'})
+
+names = [name.text for name in names]
+prices = [price.text for price in prices]
+reviews = [review.text for review in reviews]
+descriptions = [description.text for description in descriptions]
+
+table = pd.DataFrame({'Product Name': names, 'Description':descriptions, 'Reviews':reviews, 'Price':prices})
+print(table.head())
