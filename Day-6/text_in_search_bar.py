fab2619d1d4dfa19ec16ed5a29a8e0e47e2cd990
Bakar31 <abubakar1808031@gmail.com>
2022-02-01 23:06:42 +0600

    update

---
+++ Day-6/text_in_search_bar.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome('D:/Programming/Scraping/Udemy Course-1/Driver/chromedriver.exe')
+driver.get('https://www.google.com/')
