fe258467655093f95db92269ebd6dc5c6836788c
Bakar31 <abubakar1808031@gmail.com>
2022-02-01 23:15:10 +0600

    day 06

--- Day-6/text_in_search_bar.py
+++ Day-6/text_in_search_bar.py
@@ -3,3 +3,7 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome('D:/Programming/Scraping/Udemy Course-1/Driver/chromedriver.exe')
 driver.get('https://www.google.com/')
+
+input_box = driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
+input_box.send_keys('web scraping')
+input_box.send_keys(Keys.ENTER)
