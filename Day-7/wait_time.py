464ad69911fcb19ddd396dffa8d16eeeb23e01cd
Bakar31 <abubakar1808031@gmail.com>
2022-02-02 21:18:44 +0600

    day 7

--- Day-7/wait_time.py
+++ Day-7/wait_time.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_condition as EC
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 driver = webdriver.Chrome('D:/Programming/Scraping/Udemy Course-1/Driver/chromedriver.exe')
