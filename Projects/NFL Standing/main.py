7a7a846bd7f63739e6d04a59a4bca9b38ec00cb3
Bakar31 <abubakar1808031@gmail.com>
2022-02-08 00:14:21 +0600

    NFL standing

--- Projects/NFL Standing/main.py
+++ Projects/NFL Standing/main.py
@@ -1,24 +1,26 @@
-# NFL Standing Project
-
+# required libraries
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+# destination url
 url = 'https://www.espn.com/nfl/standings/_/season/2020/group/league'
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'lxml')
 
 # accessing only table html
 table = soup.find('div', class_ = 'standings__table')
-#headers elements
+
+# headers elements
 headers = table.find_all('th')
-#text headers
+
+# text headers
 headers = [th.text for th in headers][1:]
 
 # creating new dataset
 df = pd.DataFrame(columns = headers)
 
-#Team names
+# team names
 first_col = table.find('tbody', class_ = 'Table__TBODY')
 rows = first_col.find_all('tr')
 
@@ -29,7 +31,7 @@ for row in rows:
     data = data[4:]
     entities.append(data)
 
-# Other entites
+# other entites
 value_table = table.find('div', class_ = 'Table__Scroller')
 value_table_rows = value_table.find_all('tr')[1:]
 for row in value_table_rows:
@@ -38,9 +40,9 @@ for row in value_table_rows:
     length = len(df)
     df.loc[length] = single_row
 
-# Inserting team name as the first column
+# insert team name as the first column
 df.insert(loc=0, column='', value = entities)
 print(df.head())
 
-#Outputting as csv file.
+# Output as csv file.
 df.to_csv('NFL Standings 2020.csv', index = False)
